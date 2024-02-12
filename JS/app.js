05bbfd832d2bf8d7c7c389214e6354ebcb268b76
lingeaniket <linge.aniket.10@gmail.com>
2024-02-12 19:51:39 +0530

    updated dictionary

--- JS/app.js
+++ JS/app.js
@@ -1,32 +1,27 @@
 var result;
 var fId = 0;
 
-if (localStorage.getItem('history') === null) {
-    localStorage.setItem('history', "");
-}
-
-function search() {
-    let word = document.getElementById('searchWord').value;
+function search(event) {
+    event.preventDefault();
+    let word = document.getElementById("searchWord").value;
     let sWord = word.toLowerCase().trim();
     let url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + sWord;
     let response = fetch(url);
-    response.then((res) => res.json()).then((data) => getWord(data))
+    response.then((res) => res.json()).then((data) => getWord(data));
 }
 
 function getWord(data) {
-    var mainel = document.getElementById('results');
+    var mainel = document.getElementById("results");
     mainel.innerHTML = "";
     if (data.length > 0) {
         //local storage adding
-        if (localStorage.getItem('history').length > 0) {
-            var history = localStorage.getItem('history');
+        if (localStorage.getItem("history")) {
+            var history = localStorage.getItem("history");
             var array = JSON.parse(history);
             var sWord = data[0].word;
-            var isThere = array.filter(
-                (value, index, array) => {
-                    return (array[index].word === sWord);
-                }
-            );
+            var isThere = array.filter((value, index, array) => {
+                return array[index].word === sWord;
+            });
             if (array.includes(isThere[0])) {
                 var idx = array.indexOf(isThere[0]);
                 array.splice(idx, 1);
@@ -36,16 +31,15 @@ function getWord(data) {
                 obj.meaning = data[0].meanings[0].definitions[0].definition;
 
                 array.unshift(obj);
-                localStorage.setItem('history', JSON.stringify(array));
+                localStorage.setItem("history", JSON.stringify(array));
             } else {
                 var obj = {};
                 obj.word = sWord;
                 obj.meaning = data[0].meanings[0].definitions[0].definition;
 
                 array.unshift(obj);
-                localStorage.setItem('history', JSON.stringify(array));
+                localStorage.setItem("history", JSON.stringify(array));
             }
-
         } else {
             var array = [];
 
@@ -54,23 +48,36 @@ function getWord(data) {
             obj.meaning = data[0].meanings[0].definitions[0].definition;
 
             array.unshift(obj);
-            localStorage.setItem('history', JSON.stringify(array));
-
+            localStorage.setItem("history", JSON.stringify(array));
         }
 
-        var div = document.createElement('div');
-        div.innerHTML = `<span class="wordName">Word: <b><span class="nameOfWord">"` + data[0].word + `"</span></b></span>`
+        var div = document.createElement("div");
+        div.innerHTML = `<span class="wordName">Word: <b><span class="nameOfWord">"` + data[0].word + `"</span></b></span>`;
         mainel.appendChild(div);
 
         for (let i = 0; i < data[0].meanings.length; i++) {
             var el = data[0].meanings[i];
             var partOfSpeech = el.partOfSpeech;
-            var el1 = document.createElement('ol')
-            el1.innerHTML = `<b>` + partOfSpeech + `</b>`
+            var el1 = document.createElement("ol");
+            var span01 = document.createElement("div");
+            span01.className = "span-title"
+            span01.innerHTML = `Part of Speech : <b>` + partOfSpeech + `</b>`;
+            el1.appendChild(span01);
 
             for (let j = 0; j < el.definitions.length; j++) {
-                var el2 = document.createElement('li');
-                el2.innerHTML = `<span class="nameOfWord">` + el.definitions[j].definition + "</span"
+                const definition = el.definitions[j];
+                var el2 = document.createElement("li");
+                var ul1 = document.createElement("ul");
+                var li2 = document.createElement("li");
+                li2.innerHTML = `Definitions : <b>` + definition.definition + `</b>`;
+                ul1.appendChild(li2);
+                if(definition.example){
+                    var li3 = document.createElement("li");
+                    li3.innerHTML = `Examples : <b>` + definition.example + `</b>`;
+                    ul1.appendChild(li3);
+                }
+
+                el2.appendChild(ul1);
                 el1.appendChild(el2);
             }
 
@@ -78,22 +85,21 @@ function getWord(data) {
         }
 
         mainel.setAttribute("class", "bg-res");
-
     } else {
-        var errDiv1 = document.createElement('div');
-        errDiv1.setAttribute('class', "error");
+        var errDiv1 = document.createElement("div");
+        errDiv1.setAttribute("class", "error");
         errDiv1.innerHTML = "Error:- " + data.title;
         mainel.appendChild(errDiv1);
 
-        var errDiv2 = document.createElement('div');
-        errDiv2.setAttribute('class', "error");
+        var errDiv2 = document.createElement("div");
+        errDiv2.setAttribute("class", "error");
         errDiv2.innerHTML = "Message:- " + data.message;
         mainel.appendChild(errDiv2);
 
-        var errDiv3 = document.createElement('div');
-        errDiv3.innerHTML = "Resolution: -"
+        var errDiv3 = document.createElement("div");
+        errDiv3.innerHTML = "Resolution: -";
 
-        var resolutionDiv = document.createElement('div');
+        var resolutionDiv = document.createElement("div");
         resolutionDiv.innerHTML = data.resolution;
         errDiv3.appendChild(resolutionDiv);
         mainel.appendChild(errDiv3);
@@ -102,43 +108,42 @@ function getWord(data) {
     }
 }
 
-
 function historyBtn() {
-    var id1 = document.getElementById('id1').classList;
-    var id2 = document.getElementById('id2').classList;
-    var id3 = document.getElementById('id3').classList;
-    var res = document.getElementById('results').classList;
-    var his = document.getElementById('historyPage');
-    var BtnHis = document.getElementById('hisBtn');
-    var BtnBack = document.getElementById('serBtn');
+    var id1 = document.getElementById("id1").classList;
+    var id2 = document.getElementById("id2").classList;
+    var id3 = document.getElementById("id3").classList;
+    var res = document.getElementById("results").classList;
+    var his = document.getElementById("historyPage");
+    var BtnHis = document.getElementById("hisBtn");
+    var BtnBack = document.getElementById("serBtn");
 
-    id1.toggle('hide1');
-    id2.toggle('hide1');
-    id3.toggle('hide1');
-    res.toggle('hide1');
-    his.classList.toggle('hide1');
-    BtnHis.classList.toggle('hide1');
-    BtnBack.classList.toggle('hide1');
+    id1.toggle("hide1");
+    id2.toggle("hide1");
+    id3.toggle("hide1");
+    res.toggle("hide1");
+    his.classList.toggle("hide1");
+    BtnHis.classList.toggle("hide1");
+    BtnBack.classList.toggle("hide1");
 
-    var historyArr = JSON.parse(localStorage.getItem('history'));
+    var historyArr = JSON.parse(localStorage.getItem("history"));
     his.innerHTML = "";
 
     for (let i = 0; i < historyArr.length; i++) {
-        var mainDiv = document.createElement('div');
-        mainDiv.classList.add('hisItem');
-        mainDiv.setAttribute('id', `d${fId}`);
+        var mainDiv = document.createElement("div");
+        mainDiv.classList.add("hisItem");
+        mainDiv.setAttribute("id", `d${fId}`);
         mainDiv.innerHTML = "Word: " + historyArr[i].word;
 
-        var div = document.createElement('div');
-        div.classList.add('hisInnerItem');
+        var div = document.createElement("div");
+        div.classList.add("hisInnerItem");
         div.innerHTML = historyArr[i].meaning;
 
-        var div1 = document.createElement('div');
-        div1.classList.add('flex2');
+        var div1 = document.createElement("div");
+        div1.classList.add("flex2");
 
-        var btn = document.createElement('button');
-        btn.setAttribute('class', "material-icons deleteBtn");
-        btn.setAttribute('onclick', `deleteItem('${fId}')`);
+        var btn = document.createElement("button");
+        btn.setAttribute("class", "material-icons deleteBtn");
+        btn.setAttribute("onclick", `deleteItem('${fId}')`);
         btn.innerHTML = "delete";
 
         div1.appendChild(btn);
@@ -150,44 +155,42 @@ function historyBtn() {
 }
 
 function searchPage() {
-    var id1 = document.getElementById('id1').classList;
-    var id2 = document.getElementById('id2').classList;
-    var id3 = document.getElementById('id3').classList;
-    var res = document.getElementById('results');
-    var his = document.getElementById('historyPage');
-    var BtnHis = document.getElementById('hisBtn');
-    var BtnBack = document.getElementById('serBtn');
+    var id1 = document.getElementById("id1").classList;
+    var id2 = document.getElementById("id2").classList;
+    var id3 = document.getElementById("id3").classList;
+    var res = document.getElementById("results");
+    var his = document.getElementById("historyPage");
+    var BtnHis = document.getElementById("hisBtn");
+    var BtnBack = document.getElementById("serBtn");
 
     res.innerHTML = "";
     his.innerHTML = "";
 
-    id1.toggle('hide1');
-    id2.toggle('hide1');
-    id3.toggle('hide1');
-    res.classList.toggle('hide1');
-    res.classList.remove('bg-res');
-    his.classList.toggle('hide1');
-    BtnHis.classList.toggle('hide1');
-    BtnBack.classList.toggle('hide1');
+    id1.toggle("hide1");
+    id2.toggle("hide1");
+    id3.toggle("hide1");
+    res.classList.toggle("hide1");
+    res.classList.remove("bg-res");
+    his.classList.toggle("hide1");
+    BtnHis.classList.toggle("hide1");
+    BtnBack.classList.toggle("hide1");
 
-    document.getElementById('searchWord').value = "";
+    document.getElementById("searchWord").value = "";
 }
 
 function deleteItem(id) {
     var flexId = "d" + id;
 
-    var array = JSON.parse(localStorage.getItem('history'));
+    var array = JSON.parse(localStorage.getItem("history"));
 
     const element = document.getElementById(flexId);
     var word1 = element.innerHTML;
     var word = word1.replace("Word: ", "");
-    var value1 = array.filter(
-        (value, index, arr) => {
-            return (arr[index].word === word);
-        }
-    )
+    var value1 = array.filter((value, index, arr) => {
+        return arr[index].word === word;
+    });
     var idx = array.indexOf(value1[0]);
     array.splice(idx, 1);
-    localStorage.setItem('history', JSON.stringify(array));
+    localStorage.setItem("history", JSON.stringify(array));
     element.remove();
 }
